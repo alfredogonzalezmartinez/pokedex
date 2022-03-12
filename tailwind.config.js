df46ac4558a6049b13f97a2d7bbf9c2a72a0946c
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-03-12 17:17:21 +0100

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bug: '#A8B820',
+        dark: '#705848',
+        dragon: '#7038F8',
+        electric: '#F8D030',
+        fairy: '#EE99AC',
+        fighting: '#C03028',
+        fire: '#F08030',
+        flying: '#A890F0',
+        ghost: '#705898',
+        grass: '#78C850',
+        ground: '#E0C068',
+        ice: '#98D8D8',
+        normal: '#A8A878',
+        poison: '#A040A0',
+        psychic: '#F85888',
+        rock: '#B8A038',
+        steel: '#B8B8D0',
+        water: '#6890F0',
+      },
+    },
+  },
+  plugins: [],
+}
