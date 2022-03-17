749067049bf3ff3197dd4c8cf65016b341172586
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-03-17 13:39:01 +0100

    Add get functions

--- services/pokeApi.js
+++ services/pokeApi.js
@@ -1,43 +1,69 @@
 const BASE = 'https://pokeapi.co/api/v2'
 
-export const getPokemon = async (id) => {
-  if (typeof id !== 'string' && typeof id !== 'number') return null
-  const url = `${BASE}/pokemon/${id}`
+const get = async (endpoint = '', options = { id: null, offset: 0, limit: Number.MAX_SAFE_INTEGER }) => {
+  if (typeof endpoint !== 'string') return null
+  let url = `${BASE}/${endpoint}`
+
+  if (options?.id) {
+    const { id } = options
+    if (typeof id !== 'string' && typeof id !== 'number') return null
+    url = `${url}/${id}`
+  } else {
+    const offset = options?.offset && typeof options.offset === 'number' && options.offset > 0
+      ? options.offset
+      : 0
+    const limit = options?.limit && typeof options.limit === 'number' && options.limit > 0
+      ? options.limit
+      : Number.MAX_SAFE_INTEGER
+    url = `${url}?offset=${offset}&limit=${limit}`
+  }
+
   try {
-    const pokemon = await (await fetch(url)).json()
-    if (pokemon.name === 'nidoran-f') pokemon.species.name = 'Nidoran♀'
-    if (pokemon.name === 'nidoran-m') pokemon.species.name = 'Nidoran♂'
-    return pokemon
+    const data = await (await fetch(url)).json()
+    return data
   } catch (error) {
+    console.error(error)
     return null
   }
 }
 
-export const getAllPokemon = async (options = { offset: 0, limit: Number.MAX_SAFE_INTEGER }) => {
-  const offset = options?.offset && typeof options.offset === 'number' && options.offset > 0
-    ? options.offset - 1
-    : 0
-  const limit = options?.limit && typeof options.limit === 'number' && options.limit > 0
-    ? offset + options.limit
-    : Number.MAX_SAFE_INTEGER
-  const url = `${BASE}/pokedex/1/`
-  try {
-    const nationalPokedex = await (await fetch(url)).json()
-    const pokemonList = await Promise.all(
-      nationalPokedex
-        .pokemon_entries
-        .slice(offset, limit)
-        .map(({ entry_number: id }) => getPokemon(id))
-    )
-    return pokemonList
-  } catch (error) {
-    return []
-  }
+export const getAbility = async (id) => get('ability', { id })
+
+export const getAbilityList = async (options = { offset: 0, limit: Number.MAX_SAFE_INTEGER }) => {
+  const offset = options?.offset
+  const limit = options?.limit
+  return await get('ability', { offset, limit })
+}
+
+export const getEvolutionChain = async (id) => get('evolution-chain', { id })
+
+export const getPokemon = async (id) => get('pokemon', { id })
+
+export const getPokemonForm = async (id) => get('pokemon-form', { id })
+
+export const getPokemonList = async (options = { offset: 0, limit: Number.MAX_SAFE_INTEGER }) => {
+  const offset = options?.offset
+  const limit = options?.limit
+  return await get('pokemon', { offset, limit })
+}
+
+export const getPokemonSpecies = async (id) => get('pokemon-species', { id })
+
+export const getPokemonSpeciesList = async (options = { offset: 0, limit: Number.MAX_SAFE_INTEGER }) => {
+  const offset = options?.offset
+  const limit = options?.limit
+  return await get('pokemon-species', { offset, limit })
 }
 
 const pokeApi = {
-  getAllPokemon,
+  getAbility,
+  getAbilityList,
+  getEvolutionChain,
   getPokemon,
+  getPokemonForm,
+  getPokemonList,
+  getPokemonSpecies,
+  getPokemonSpeciesList,
 }
 
 export default pokeApi
