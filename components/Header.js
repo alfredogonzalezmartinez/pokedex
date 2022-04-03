f3f14b7b4ef0dbbab9b4bf9db1520b395d8b0a27
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-04-03 19:23:04 +0200

    Add max width for content

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,16 @@ import logo from '../public/pokeball.svg'
 
 const Header = () => {
   return (
-    <header className='flex flex-col sm:flex-row justify-center sm:justify-between place-content-center p-3 bg-pokedex text-md text-white mb-3'>
-      <Link href="/">
-        <a className='flex justify-center items-center gap-2 p-2'>
-          <Image src={logo} alt='logo' height={40} width={40}/>
-          <h1 className='text-5xl font-bold'>Pokédex</h1>
-        </a>
-      </Link>
-      <Searcher/>
+    <header className='p-3 bg-redPokedex text-white'>
+      <div className='flex flex-col justify-center sm:flex-row sm:justify-between max-w-screen-2xl mx-auto'>
+        <Link href="/">
+          <a className='flex justify-center items-center gap-2 p-2'>
+            <Image src={logo} alt='logo' height={40} width={40}/>
+            <h1 className='text-5xl font-bold'>Pokédex</h1>
+          </a>
+        </Link>
+        <Searcher/>
+      </div>
     </header>
   )
 }
