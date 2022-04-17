cae75fc8f5a2bc1d4f35b000a786feab21dba49a
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-04-17 12:03:26 +0200

    Replace default image with substitute Image

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,16 +1,20 @@
+import { capitalize } from '../services/utils/capitalize'
+
 import ImageWithBackground from './ImageWithBackground'
 import PokemonTypeBadge from './PokemonTypeBadge'
 
-import defaultImage from '../public/default.png'
+import substitute from '../public/substitute.png'
 
-const PokemonCard = ({ name = '', number = 0, sprite = defaultImage, types = [] }) => {
+const PokemonCard = ({ name = 'substitute', number = 0, sprite = substitute, types = ['unknown'] }) => {
+  name = capitalize(name)
+
   return (
     <article className='p-4 border-solid border  border-gray-300  rounded-lg bg-white'>
       <ImageWithBackground
         src={sprite}
         alt={`${name} sprite`}
       />
-      <h2 className='text-4xl capitalize font-semibold flex justify-between my-2'>
+      <h2 className='text-4xl font-semibold flex justify-between my-2'>
         {name}
         <span className='text-2xl text-gray-400'>#{number}</span>
       </h2>
