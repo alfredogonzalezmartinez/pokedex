32eab3ba7d25a813b0cf330e720baaa8090d890d
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-05-23 11:34:15 +0200

    Change /api/pokemon-card endpoint to get url from the request

--- pages/api/pokemon-cards.js
+++ pages/api/pokemon-cards.js
@@ -6,10 +6,14 @@ export default async function handler (req, res) {
   const requestMethod = req.method
   if (!ALLOWED_METHODS.includes(requestMethod)) res.status(405).json({ error: `Method ${requestMethod} Not Allowed` })
 
-  const limit = req.query?.limit
-  const offset = req.query?.offset
+  const protocol = req.headers['x-forwarded-proto'] || req.headers.referer?.split('://')[0] || 'http'
+  const host = req.headers.host
+  const url = `${protocol}://${host}`
 
-  const data = await getPokemonCards({ offset, limit })
+  const limit = req.query.limit
+  const offset = req.query.offset
+
+  const data = await getPokemonCards({ offset, limit, url })
 
   res.status(200).json(data)
 }
